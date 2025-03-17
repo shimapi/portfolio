4b0d8b505137d8637d42cb32a0cb0a82feabe9d9
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2025-03-17 04:50:33 -0300

    feat: adding style to project page :D

--- src/components/Projects/ProjectPage.jsx
+++ src/components/Projects/ProjectPage.jsx
@@ -15,31 +15,44 @@ const ProjectPage = () => {
 
 	return (
 		<div className="project-page">
-			<h1>{project.title}</h1>
-			<h3>{project.category}</h3>
-			<section className="project-page__client">
-				<strong>{t("general.client")}: </strong> {project.client}
-			</section>
-			<section className="project-page__date">
-				<strong>{t("general.date")}: </strong> {project.date}
-			</section>
-			<section className="project-page__tech">
-				<strong>{t("general.tech")}: </strong>
-				{Object.values(project.tech).map((tech, i) => (
-					<span key={i} className="tech-tag">{tech}</span>
-				))}
-			</section>
-			<section className="project-page__details">
-				<strong>{t("general.details")}: </strong>
-				<ul>
-					{Object.values(project.bullets).map((bullet, i) => (
-						<li key={i}>{bullet}</li>
-					))}
-				</ul>
-			</section>
+			<h2>{project.title}</h2>
 			
+			<article className="project-page__content">
+				<section className="project-page__timeline">
+					<div className="project-page__timeline-line"></div>
+					<div className="project-page__timeline-circle"></div>
+				</section>
+
+				<section className="project-page__info">
+					<h4 className="project-page__info-category">{project.category}</h4>
+					<h5 className="project-page__info-metadata">
+						<span className="project-page__info-metadata-span">
+							{project.client}
+						</span>
+						<span className="project-page__info-metadata-span">
+							{project.date}
+						</span>
+					</h5>
+
+
+
+					<ul className="project-page__info-description">
+						{Object.values(project.bullets).map((bullet, i) => (
+							<li key={i} className="project-page__info-bullet">{bullet}</li>
+						))}
+					</ul>
+
+					<div className="project-page__info-tech">
+						{Object.values(project.tech).map((tech, i) => (
+							<span key={i} className="project-card__tech-unit box">{tech}</span>
+						))}
+					</div>
+					
+				</section>
+			</article>
+
 			<section className="project-page__gallery">
-				<h3>{t("general.gallery")}</h3>
+				<h3 className="subtitle">{t("general.gallery")}</h3>
 				<div className="gallery-grid">
 					{Object.values(project.img).map((img, i) => (
 						<div key={i} className="gallery-item">
