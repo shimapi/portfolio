9dbf69d824f9992de17f9c5903000da285d55d00
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2025-03-17 04:58:44 -0300

    feat: changing dark theme color for footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import { Home } from "./pages/Home";
 import { useContext, useEffect } from "react";
@@ -14,6 +14,17 @@ import Projects from "./components/Projects/Projects";
 import PathConstants from "./routes/pathConstants";
 import ProjectPage from "./components/Projects/ProjectPage";
 
+// Componente para controlar el scroll
+function ScrollToTop() {
+	const { pathname } = useLocation();
+	
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+	
+	return null;
+}
+
 const App = () => {
 	const { theme } = useContext(ThemeContext);
 
@@ -23,6 +34,7 @@ const App = () => {
 
 	return (
 		<BrowserRouter>
+			<ScrollToTop />
 			<Header />
 			<main className={`container main__container`}>
 				<Routes>
